63321458b9bceb7dbd7e8b88cebdf1f1130331e2
Sudhir Rathod <125038605+sudhirrathod03@users.noreply.github.com>
2026-04-15 13:39:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,58 @@
 //your JS code here. If required.
+let userInput = document.getElementById("ip")
+let btn = document.getElementById("btn")
+let displayOutput = document.getElementById("output")
+
+function makePromise(){
+	let value = new Promise((resolve)=>{
+		let num = parseInt(userInput.value);
+		resolve(num);
+	})
+	value.then((num)=>{
+		return new Promise((resolve)=>{
+			setTimeout(()=>{
+				displayOutput.textContent = num;
+				resolve(num)
+			},2000)
+		})
+	}).then((num)=>{
+		return new Promise((resolve)=>{
+			setTimeout(()=>{
+				num = num * 2
+				displayOutput.textContent = num;
+				resolve(num)
+			},1000)
+		})
+	}).then((num)=>{
+		return new Promise((resolve)=>{
+			setTimeout(()=>{
+				num = num-3
+				displayOutput.textContent = num;
+				resolve(num)
+			},1000)
+		})
+	}).then((num)=>{
+		return new Promise((resolve)=>{
+			setTimeout(()=>{
+				num = num / 2
+				displayOutput.textContent = num;
+				resolve(num)
+			},1000)
+		})
+	}).then((num)=>{
+		return new Promise((resolve)=>{
+			setTimeout(()=>{
+				num = num + 10
+				displayOutput.textContent = num;
+				resolve(num)
+			},1000)
+		})
+	})
+}
+
+
+
+btn.addEventListener("click",makePromise)
+
+
+
